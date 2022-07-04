35cc63cb402379753e3e454e42ab963f0f61ebab
Kirill Kazakov <kirill_kazakov2003@mail.ru>
2022-07-04 20:13:45 +0300

    keyboard shit

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
+
 
 vk_session = vk_api.VkApi(token="vk1.a.K6eYXuEK8BY8qUaVGLIksmTeSYjgmkcDhllTUT58MrmIusZF1xFqeEn62C496gpojPfE67FKDFQrWmQfqecOZv68a3FYtCAEj3zGB-lz6kc-V4Lozuv8cMQ0Hznn0k8fZJU5Coh1l1Fdy7NO6uXcacg3sGzo38Idk_OUWfJSjTCKt0IiEf818TlmCrYUXBMp")
-session_api = vk_session.get_api()
+vk = vk_session.get_api()
 longpool = VkLongPoll(vk_session)
+
+keyboard = VkKeyboard(one_time=True)
+
+keyboard.add_button('Отправить свое местоположение в ФБР', color=VkKeyboardColor.SECONDARY)
+keyboard.add_line()
+keyboard.add_button('Отправить свое местоположение в ФСБ', color=VkKeyboardColor.POSITIVE)
+# Настройки для обоих клавиатур
+settings = dict(one_time=False, inline=True)
 
 def send_some_msg(id, some_text):
     vk_session.method("messages.send", {"user_id":id, "message":some_text,"random_id":0})
@@ -13,6 +24,7 @@ for event in longpool.listen():
         if event.to_me:
             msg = event.text.lower()
             id = event.user_id
-
+            print(msg);
             if msg == "начать":
                 send_some_msg(id, 'TATTS - бренд временных татуировок и накладных ноготочков. \r\n\r\n Мы есть на Ozon и Wildberries, тыкай меню, чтобы увидеть ссылки. \r\n\r\n Тут отчёт наш админ, а ещё подключен бот, для ответа на популярные вопросы. \r\n\r\n Запутаешься, пиши "Меню" ')
+            else: vk.messages.send(peer_id=id,message="Ну привет! Я бот! Хочешь, отправлю историю твоего браузера в правоохранительные органы?",keyboard=keyboard.get_keyboard(),random_id=0)
